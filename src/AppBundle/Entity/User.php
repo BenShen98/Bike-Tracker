847f3914774464a2b2694642026d9de10bf67a83
Ben <shenleyang007@gmail.com>
2016-03-22 21:25:10 +0000

    prepare to use environment variables

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * User
  *
- * @ORM\Table(name="EESuser")
+ * @ORM\Table(name="eesuser")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\Entity\UserRepository")
  */
 class User implements UserInterface, \Serializable
