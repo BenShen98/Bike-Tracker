99cf6d71e4295ce736c7f7c474125fc78aeed4d5
Ben <shenleyang007@gmail.com>
2016-04-14 19:12:21 +0000

    x

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -21,18 +22,24 @@ class SecurityController extends Controller
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render(
-            'security/login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $lastUsername,
-                'error'         => $error,
-            )
-        );
+        if ($error == null)
+        {
+            $response = new RedirectResponse($this->generateUrl('about'));
+            return $response->send();
+        }else{
+
+            return $this->render(
+                'security/login.html.twig',
+                array(
+                    // last username entered by the user
+                    'last_username' => $lastUsername,
+                    'error'         => $error,
+                )
+            );
+        }
     }
 
 
